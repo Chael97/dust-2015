3f7c215695e8fddb7a17e9bec6693466927103ba
Roxana Hickey <roxana.hickey@gmail.com>
2015-10-28 22:05:08 -0700

    Update files after removing additional chloroplast contaminants

--- code/ger_summary_plots.R
+++ code/ger_summary_plots.R
@@ -9,39 +9,33 @@ library(vegan)
 library(labdsv)
 library(plyr)
 library(phyloseq)
+library(gplots)
 
 ## load custom functions
-source('code/custom_ggplot_settings.R')
-source('code/custom_heatmap.3.R')
+# source('code/custom_ggplot_settings.R')
+# source('code/custom_heatmap.3.R')
 
 ## load OTU table and map file from setup
 load('results/otu_setup/ger_otu_setup.RData')
 
 ## heatmap
+## write dataframe with shorter taxa names
+df.taxa <- data.frame(full = colnames(otu.g.prop),
+                      short = '')
+
 ## subset to top 50 taxa
-# pick <- order(colSums(otu.g), decreasing=TRUE)[1:25]
-# heatmap(otu.g[,pick], trace = NULL, margins = c(15,5))
+pick <- order(colSums(otu.g.prop), decreasing=TRUE)[1:50]
+heatmap(otu.g.prop[, pick], trace = NULL)
 
-## bar plots
-
-## compute Canberra and Bray-Curtis distances on rarefied data
-can.g <- vegdist(otu.g.rare, 'canberra')
+####################################
+## compute Bray-Curtis and Canberra distances on rarefied data
 bc.g <- vegdist(otu.g.rare)
+can.g <- vegdist(otu.g.rare, 'canberra')
 
+####################################
 ## PCoA
-pcoa.can.g <- cmdscale(t(can.g), eig = TRUE)
-# png('figures/pcoa_genus_can.png', width = 6, height = 6, units = 'in', res = 300)
-# ordiplot(scores(pcoa.can.g)[,c(1,2)], type = 't', 
-         # main = 'PCoA with genera (Canberra distance)',
-         # xlab = 'PCoA 1', ylab = 'PCoA 2')
-# dev.off()
-
 pcoa.bc.g <- cmdscale(t(bc.g), eig = TRUE)
-# png('figures/pcoa_genus_bc.png', width = 6, height = 6, units = 'in', res = 300)
-# ordiplot(scores(pcoa.bc.g)[,c(1,2)], type = 't', 
-         # main = 'PCoA with genera (Bray-Curtis dissimilarity)',
-         # xlab = 'PCoA 1', ylab = 'PCoA 2')
-# dev.off()
+pcoa.can.g <- cmdscale(t(can.g), eig = TRUE)
 
 ####################################
 ## plot Bray-Curtis pcoa with ggplot
@@ -50,46 +44,50 @@ colnames(df.pcoa.bc.g) <- c('PCoA1', 'PCoA2')
 df.pcoa.bc.g$Description <- rownames(df.pcoa.bc.g)
 df.pcoa.bc.g.all <- merge(df.pcoa.bc.g, ger.map)
 
+## specify 12 colors for space types
+mycol.12 <- c('#A6CEE3', '#1F78B4', '#B2DF8A', '#33A02C', '#FB9A99', '#E31A1C', 
+              '#FDBF6F', '#FF7F00', '#CAB2D6', '#6A3D9A', '#EEEE00', '#B15928')
+
 ## all samples by space type
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE))
 gg.pcoa.bc.g + geom_point(size = 2) +
   geom_text(aes(y = PCoA2 + 0.01, label = Description), size = 2, vjust = 0) + 
-  scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+  scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_sampletype.png')
 
 ## crack area NSF
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = CrackAreaNSF))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_crackarea.png')
 
 ## chem = TCSavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = TCSavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_TCSavg.png')
 
 ## chem = TCCavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = TCCavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_TCCavg.png')
 
 ## chem = MePBavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = MePBavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_MePBavg.png')
 
 ## chem = EtPBavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = EtPBavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_EtPBavg.png')
 
 ## chem = PrPBavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = PrPBavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_PrPBavg.png')
 
 ## chem = BuBPavg  
 gg.pcoa.bc.g <- ggplot(df.pcoa.bc.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = BuPBavg))
-gg.pcoa.bc.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.bc.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.bc.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_bc_g_BuPBavg.png')
 
 #################################
@@ -103,40 +101,40 @@ df.pcoa.can.g.all <- merge(df.pcoa.can.g, ger.map)
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE))
 gg.pcoa.can.g + geom_point(size = 2) +
   geom_text(aes(y = PCoA2 + 0.01, label = Description), size = 2, vjust = 0) + 
-  scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+  scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_sampletype.png')
 
 ## crack area NSF
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = CrackAreaNSF))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_crackarea.png')
 
 ## chem = TCSavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = TCSavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_TCSavg.png')
 
 ## chem = TCCavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = TCCavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_TCCavg.png')
 
 ## chem = MePBavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = MePBavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_MePBavg.png')
 
 ## chem = EtPBavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = EtPBavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_EtPBavg.png')
 
 ## chem = PrPBavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = PrPBavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_PrPBavg.png')
 
 ## chem = BuBPavg  
 gg.pcoa.can.g <- ggplot(df.pcoa.can.g.all, aes(x = PCoA1, y = PCoA2, color = SpaceTypeBioBE, size = BuPBavg))
-gg.pcoa.can.g + geom_point() + scale_color_manual(values = brewer.pal(length(unique(df.pcoa.can.g.all$SpaceTypeBioBE)), 'Paired'))
+gg.pcoa.can.g + geom_point() + scale_color_manual(values = mycol.12)
 ggsave('figures/pcoa_can_g_BuPBavg.png')
---
+++ data/map_files/README.txt
@@ -0,0 +1,8 @@
+151027_map_edit_finalchems_crack_plus.txt
+- received from Erica on 10/27/2015
+
+151028_map_edit_finalchems_crack_plus.txt
+- copied from 151027_map_edit_finalchems_crack_plus.txt to make modifications
+- modified on 10/28/15 by RJH to remove apostrophes (e.g. men’s.lockerroom) because they caused problems with loading in R
+- modified on 10/28/15 by RJH to add “neg.control” for control samples under categories describing sample type
+- modified on 10/28/15 by RJH to add “hallway” for sample oneuL2181 under category “SampleTypeBioBE”
