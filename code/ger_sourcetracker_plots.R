bccb46018f02102c9eefe1d78e22e385661ab8eb
Roxana Hickey <roxana.hickey@gmail.com>
2015-11-11 00:17:16 -0800

    Update contaminant removal script, summary plots script and figures

--- code/ger_sourcetracker_plots.R
+++ code/ger_sourcetracker_plots.R
@@ -26,23 +26,37 @@ prop.pick <- c('Env', 'TITLE', 'Proportion_Human.Feces', 'Proportion_Human.Mouth
 res.prop <- map.out[map.out$SourceSink == 'sink', prop.pick]
 res.prop$SampleID <- rownames(res.prop)
 
-## make barplots
+## melt dataframe for barplots
 res.prop.lg <- melt(res.prop, id.vars = c('SampleID', 'Env', 'TITLE'))
 
-# gg.prop.bar <- ggplot(res.prop.lg, aes(x = SampleID, y = value, fill = variable))
-# gg.prop.bar + geom_bar(stat = 'identity') +
-gg.prop.box <- ggplot(res.prop.lg, aes(x = TITLE, y = value, fill = variable))
+## narrow selection to Gerlinger only
+res.prop.lg.ger <- subset(res.prop.lg, TITLE == 'Gerlinger')
+sample.pick.ger <- rownames(map.out)[grep('oneuL', rownames(map.out))]
+
+## note control samples
+control.id <- rownames(map.out)[grep('oneuLcontroL', rownames(map.out))]
+
+## set sample order for plots
+ord.id <- c(rownames(map.out)[order(map.out$Env)][rownames(map.out)[order(map.out$Env)] %in% sample.pick.ger[!(sample.pick.ger %in% control.id)]],
+            control.id)
+ord.env <- c(as.character(map.out$Env[order(map.out$Env)][rownames(map.out)[order(map.out$Env)] %in% sample.pick.ger[!(sample.pick.ger %in% control.id)]]),
+             rep('control', 2))
+
+## bar plot of Gerlinger-only sourcetracker results
+gg.prop.bar <- ggplot(res.prop.lg.ger, aes(x = SampleID, y = value, fill = variable))
 gg.prop.bar + geom_bar(stat = 'identity') +
-  scale_fill_manual(values = c(brewer.pal(9, 'YlOrRd')[c(8,7,5,3,1)], 
+  scale_fill_manual(values = c(brewer.pal(11, 'Spectral')[c(11,10,9,8,7)], 
                                'grey90'),
                     labels = c('Human Feces', 'Human Mouth', 'Human Skin',
                                'Human Urine', 'Human Vagina', 'Unknown'),
                     name = 'Putative Source') +
+  scale_x_discrete(limits = rev(ord.id), 
+                   labels = rev(ord.env)) +
+  xlab('Room Type') +
   ylab('Proportion') +
   coord_flip() +
   theme_bw()
-  # theme(axis.text.x = element_text(angle = -45, hjust = 0))
-# ggsave('figures/ger_wood_barberan_sourcetracker_genus.png', width = 8, height = 10)
+ggsave('figures/ger_sourcetracker_genus.png', width = 8, height = 10)
 
 ## summarize all human together
 res.prop.2 <- res.prop
@@ -54,26 +68,9 @@ res.prop.2$Proportion_Human <- rowSums(res.prop.2[ , c('Proportion_Human.Feces',
 res.prop.2 <- res.prop.2[ , c('Env', 'TITLE', 'SampleID', 
                               'Proportion_Human', 'Proportion_Unknown')]
 
-res.prop.2.lg <- melt(res.prop.2, id.vars = c('SampleID', 'Env', 'TITLE'))
-  
-gg.prop.2.box <- ggplot(res.prop.2.lg, aes(x = TITLE, y = value, fill = variable))
-gg.prop.2.box + geom_boxplot() + 
-  scale_fill_manual(values = c('dodgerblue', 'yellow4'),
-                    labels = c('Human', 'Unknown'),
-                    name = 'Putative Source') +
-  scale_x_discrete(limits = c('Gerlinger',                                                               
-                              'Athletic equipment microbiota are shaped by interactions with human skin',
-                              'The ecology of microscopic life in household dust'), 
-                   labels = c('Gerlinger',
-                              'Athletic Facilities',
-                              'Homes')) +
-  xlab('Study') +
-  ylab('Proportion') +
-  theme_bw()
-ggsave('figures/ger_wood_barberan_sourcetracker_genus_boxplot.png', width = 6, height = 8)
-
+## density plot of 3 studies
 gg.prop.2.hist <- ggplot(res.prop.2, aes(x = Proportion_Human, fill = TITLE))
-gg.prop.2.hist + geom_density(alpha = 0.5, adjust = 0.75) +
+gg.prop.2.hist + geom_density(alpha = 0.6, adjust = 0.75) +
   # geom_line(stat = 'density', adjust = 0.75) +
   scale_fill_manual(values = c('darkgoldenrod4', 'yellow2', 'cadetblue2'),
                     limits = c('Gerlinger',                                                               
@@ -91,3 +88,53 @@ gg.prop.2.hist + geom_density(alpha = 0.5, adjust = 0.75) +
   theme(legend.key = element_rect(colour = 'black'), legend.background = element_blank(),
         legend.position = c(1,1), legend.justification = c(1,1))
 ggsave('figures/ger_wood_barberan_sourcetracker_genus_density.png', width = 6, height = 6)
+
+## violin plot of 3 studies
+gg.prop.2.vio <- ggplot(res.prop.2, aes(x = TITLE, y = Proportion_Human, fill = TITLE))
+gg.prop.2.vio + geom_violin(adjust = 0.75) +
+  geom_boxplot(width = 0.1, fill = 'black', outlier.color = NA) +
+  stat_summary(fun.y = median, geom = 'point', fill = 'white', shape = 21, size = 2.5) +
+  scale_fill_manual(values = c('darkgoldenrod4', 'yellow2', 'cadetblue2'),
+                    limits = c('Gerlinger',                                                               
+                               'Athletic equipment microbiota are shaped by interactions with human skin',
+                               'The ecology of microscopic life in household dust'), 
+                    labels = c('Gerlinger',
+                               'Athletic Facilities',
+                               'Homes'),
+                    name = 'Study') +
+  scale_x_discrete(limits = c('Gerlinger',                                                               
+                              'Athletic equipment microbiota are shaped by interactions with human skin',
+                              'The ecology of microscopic life in household dust'), 
+                   labels = c('Gerlinger',
+                              'Athletic Facilities',
+                              'Homes')) +
+  xlab('Proportion of putative human-sourced bacteria') + 
+  ylab('Frequency') +
+  guides(fill = guide_legend(override.aes = list(colour = NULL))) +
+  theme_bw() +
+  theme(legend.key = element_rect(colour = 'black'), legend.background = element_blank(),
+        legend.position = c(1,1), legend.justification = c(1,1))
+ggsave('figures/ger_wood_barberan_sourcetracker_genus_violin.png', width = 6, height = 6)
+
+
+## box plot of 3 studies
+# gg.prop.2.box <- ggplot(res.prop.2, aes(x = TITLE, y = Proportion_Human, fill = TITLE))
+# gg.prop.2.box + geom_boxplot(show_guide = FALSE) + 
+#   scale_fill_manual(values = c('darkgoldenrod4', 'yellow2', 'cadetblue2'),
+#                     limits = c('Gerlinger',                                                               
+#                                'Athletic equipment microbiota are shaped by interactions with human skin',
+#                                'The ecology of microscopic life in household dust'), 
+#                     labels = c('Gerlinger',
+#                                'Athletic Facilities',
+#                                'Homes'),
+#                     name = 'Study') +
+#   scale_x_discrete(limits = c('Gerlinger',                                                               
+#                               'Athletic equipment microbiota are shaped by interactions with human skin',
+#                               'The ecology of microscopic life in household dust'), 
+#                    labels = c('Gerlinger',
+#                               'Athletic Facilities',
+#                               'Homes')) +
+#   xlab('Study') +
+#   ylab('Proportion of putative human-sourced bacteria') +
+#   theme_bw()
+# ggsave('figures/ger_wood_barberan_sourcetracker_genus_boxplot.png', width = 6, height = 8)
